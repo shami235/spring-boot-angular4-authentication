ed1f08ecd327af7b536af35b52e7eaf90bf206d4
unknown <shaz_mailme@yahoo.com>
2018-05-17 01:14:31 +1000

    Revert "added material UI and bugfix"
    This reverts commit 98c88bb91438d8d8990a94433395630a75933a0d.

--- front-end/src/app/services/account.service.ts
+++ front-end/src/app/services/account.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {User} from "../model/model.user";
 import {Http} from "@angular/http";
 import {AppComponent} from "../app.component";
-import {Headers, RequestOptions,Response} from '@angular/http';
+import { Http, Headers, RequestOptions,Response} from '@angular/http';
 
 @Injectable()
 export class AccountService {
@@ -16,8 +16,8 @@ export class AccountService {
   addNumbers(){
     return this.http.post(AppComponent.API_URL+'/home/calculate',10)
       .map((response: Response) => {
-        console.log(response)
-        })
+        console.log(response._body)
+      }
       /*.map(resp=>resp.json())
       .subscribe(
         function(response) { console.log("Success Response" + response)},
